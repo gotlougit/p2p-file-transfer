abc2626e256954a78fd6c1d28d4ddcce77c61d6c
Saksham Mittal <gotlouemail@gmail.com>
2022-10-23 22:40:46 +0530

    Consolidate client and server in one and modularize client code

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 use std::env;
 use std::fs::File;
 use std::io::Read;
+use std::io::Write;
 use std::net::UdpSocket;
 use std::str;
 use std::sync::Arc;
@@ -21,6 +22,27 @@ fn get_file_to_serve(filename: &String) -> Arc<Vec<u8>> {
     Arc::new(data)
 }
 
+fn save_data_to_file(socket : UdpSocket, file : &mut File) {
+    let mut buf = [0u8; MTU];
+    loop {
+        let (amt, _) = socket
+            .recv_from(&mut buf)
+            .expect("Couldn't read from socket!");
+        if amt < MTU {
+            match file.write_all(&buf[..amt]) {
+                Ok(v) => v,
+                Err(e) => eprint!("Encountered an error while writing: {}", e),
+            };
+            break;
+        } else {
+            match file.write_all(&buf) {
+                Ok(v) => v,
+                Err(e) => eprint!("Encountered an error while writing: {}", e),
+            };
+        }
+    }
+}
+
 fn send_data_in_chunks(sock: UdpSocket, src: std::net::SocketAddr, data: Arc<Vec<u8>>) {
     println!("Got connection from {}", src);
     let mut start: usize = 0;
@@ -36,13 +58,12 @@ fn send_data_in_chunks(sock: UdpSocket, src: std::net::SocketAddr, data: Arc<Vec
 }
 
 //will be useful to help implement authentication
-fn is_valid_request(request_body : [u8; MTU], validreq : &String) -> bool {
-     let req = 
-         String::from(str::from_utf8(&request_body).expect("Couldn't write buffer as string"));
+fn is_valid_request(request_body: [u8; MTU], validreq: &String) -> bool {
+    let req = String::from(str::from_utf8(&request_body).expect("Couldn't write buffer as string"));
     req[..validreq.len()].eq(validreq)
 }
 
-fn serve(filename : &String) {
+fn serve(filename: &String) {
     //server only responds to requests with this particular body
     let validreq = String::from("GET ") + filename + "\n";
 
@@ -82,11 +103,47 @@ fn serve(filename : &String) {
     }
 }
 
+fn client(server_interface: &String, file_to_get: &String, filename: &String) {
+    //test values, will be dynamic later on
+    let interface = "0.0.0.0:8000";
+
+    //way to get the server to serve a particular file
+    let request = String::from("GET ") + file_to_get + "\n";
+    let rawreq = request.as_bytes();
+
+    //keep file open for writing to interface
+    let mut file = File::create(filename).expect("Couldn't create file!");
+
+    //open socket and start networking!
+    let socket = UdpSocket::bind(interface).expect("Couldn't bind to specified port!");
+    socket
+        .connect(server_interface)
+        .expect("Couldn't connect to server, is it running?");
+
+    socket.send(rawreq).expect("Couldn't write to server!");
+
+    //keep reading till there's nothing left to read
+    save_data_to_file(socket, &mut file);
+    println!("Wrote received data to {}", filename);
+}
+
 fn main() {
     let args: Vec<String> = env::args().collect();
-    if args.len() != 2 {
-        panic!("Insufficient args entered! USAGE: ./server <filenametotransmit>");
+    if args.len() < 2 {
+        eprintln!("Insufficient args entered! Usage: ./program client <server_interface> <file_to_get> <filename> or ./program server <file_to_serve>");
     }
-    let filename = &args[1];
-    serve(filename); 
+
+    let mode = &args[1];
+
+    if mode == "server" {
+        let filename = &args[2];
+        serve(filename);
+    } else if mode == "client" {
+        let server_interface = &args[2];
+        let file_to_get = &args[3];
+        let filename = &args[4];
+        client(server_interface, file_to_get, filename);
+    } else {
+        eprintln!("Incorrect args entered!");
+    }
 }
