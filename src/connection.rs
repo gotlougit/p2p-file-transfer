5bfb1c7944926c671056331717a3935b7089961e
Saksham Mittal <gotlouemail@gmail.com>
2023-01-10 00:25:54 +0530

    Initial replacement of protocol.rs & logging debut
    Currently, protocol.rs relies too much on Mutexes and is optimized for a
    single connection, and was getting a bit too disorganized and unwieldy.
    To fix this, a split is proposed into parsing.rs and connection.rs
    
    - parsing.rs: takes care of each message type, *only* this file should
      be responsible for converting between message types/data and raw bytes
      to be sent to the network. It uses Options quite extensively to signal
      externally that message may not be right.
    
    - connection.rs: provides a dumb pipe to each IP you want to connect to
      along with basic NAT traversal techniques. It also has a
      reliable_recv() function which is an Option depending on if some data
      was acquired in a given time period or not
    
    These 2 files also debut using proper logging utils in the project,
    which was an oft-neglected chore I was putting off, but which will
    eventually make its way into the rest of the project

---
+++ src/connection.rs
@@ -0,0 +1,231 @@
+//provide basic NAT traversal and reliable data transfer abstractions
+use std::collections::HashMap;
+use std::hash::Hash;
+use std::net::SocketAddr;
+use std::thread;
+use std::time::SystemTime;
+use std::time::{Duration, UNIX_EPOCH};
+use tokio::net::UdpSocket;
+use tokio::time::timeout;
+
+use log::debug;
+use log::error;
+use log::info;
+use log::warn;
+
+//useful to mark states of connection
+pub enum ClientState {
+    NoState,
+    ACKorNACK,
+    SendFile,
+    EndConn,
+    EndedConn,
+}
+
+//defines how small and large the sliding window can be
+const INITIAL_N: usize = 6;
+const MAX_N: usize = 75;
+
+//DIVIDER tells the maximum time that both machines will wait before initiating
+//NAT traversal
+const DIVIDER: u64 = 15;
+
+//amount of time each machine waits before declaring a timeout and initiating
+//RESEND
+const MAX_WAIT_TIME: Duration = Duration::from_secs(5);
+
+//MTU: maximum raw info in one packet
+pub const MTU: usize = 1280;
+//DATA_SIZE: amount of file each packet will contain
+pub const DATA_SIZE: usize = 1000;
+
+//number of dummy messages to send
+const DUMMY_MSG_NUM: usize = 5;
+
+//small function used everywhere here
+fn change_map_value<K, V>(map: &mut HashMap<K, V>, key: K, newval: V)
+where
+    K: Eq,
+    K: Hash,
+{
+    if map.remove(&key).is_some() {
+        map.insert(key, newval);
+    }
+}
+
+//main abstraction to handle connections
+pub struct Connection {
+    socket: UdpSocket,
+    protocol_n: HashMap<SocketAddr, usize>,
+    encryption_token: HashMap<SocketAddr, String>,
+    lastmsg: HashMap<SocketAddr, Vec<Vec<u8>>>,
+}
+
+pub fn init_conn(socket: UdpSocket) -> Connection {
+    Connection {
+        socket,
+        protocol_n: HashMap::new(),
+        encryption_token: HashMap::new(),
+        lastmsg: HashMap::new(),
+    }
+}
+
+impl Connection {
+    //helps sync NAT traversal
+    pub fn sync_nat_traversal(&self) {
+        //wait till some common time
+        let time_to_wait = DIVIDER
+            - (SystemTime::now()
+                .duration_since(UNIX_EPOCH)
+                .unwrap()
+                .as_secs()
+                % DIVIDER);
+        info!("Waiting for {} seconds", time_to_wait);
+        thread::sleep(Duration::from_secs(time_to_wait));
+    }
+
+    //sends dummy packets to other machine and returns connection state
+    pub async fn init_nat_traversal(&self, ip: &SocketAddr) -> bool {
+        let mut connected = false;
+        let dummymsg = *b"HELLOWORLD";
+        for _ in 0..DUMMY_MSG_NUM {
+            self._send_to(ip, &dummymsg).await;
+            let mut buf = [0u8; MTU];
+            let recv_future = self._recv(&mut buf);
+            match timeout(MAX_WAIT_TIME, recv_future).await {
+                Ok((_, src)) => {
+                    connected = connected || src == *ip;
+                    info!("Seemed to get data from {}", src);
+                }
+                Err(_) => {
+                    warn!("Did not receive any data, retrying...");
+                }
+            }
+        }
+        connected
+    }
+
+    //deal with N for each IP
+    fn grow_n(&mut self, ip: &SocketAddr) {
+        if let Some(n) = self.protocol_n.get(ip) {
+            if n * 2 <= MAX_N {
+                let n = &(n * 2);
+                debug!("Increased N to {} for IP: {}", n, ip);
+                change_map_value::<SocketAddr, usize>(&mut self.protocol_n, *ip, *n);
+                return;
+            }
+        }
+        error!("N could not be read for IP {}, probably not in map", ip);
+    }
+
+    fn reset_n(&mut self, ip: &SocketAddr) {
+        if let Some(n) = self.protocol_n.get(ip) {
+            let n = &INITIAL_N;
+            debug!("Reset N to {} for IP: {}", n, ip);
+            change_map_value::<SocketAddr, usize>(&mut self.protocol_n, *ip, *n);
+            return;
+        }
+        error!("N could not be read for IP {}, probably not in map", ip);
+    }
+
+    pub fn read_n(&self, ip: &SocketAddr) -> usize {
+        if let Some(n) = self.protocol_n.get(ip) {
+            debug!("Read N as {} for IP: {}", n, ip);
+            return *n;
+        }
+        error!("N could not be read for IP {}, probably not in map", ip);
+        0
+    }
+
+    //get current list of IP addresses connected
+    fn get_ip_connected(&self) -> Vec<SocketAddr> {
+        let iplist = self.protocol_n.keys().cloned().collect::<Vec<SocketAddr>>();
+        return iplist;
+    }
+
+    //deal with last messages for each IP
+    fn add_last_msg(&mut self, ip: &SocketAddr, message: Vec<u8>) {
+        if let Some(v) = self.lastmsg.get(ip) {
+            if v.len() == self.read_n(ip) {
+                self.reset_last_msg(ip);
+            }
+            return;
+        }
+        error!("N could not be read for IP {}, probably not in map", ip);
+    }
+
+    fn reset_last_msg(&mut self, ip: &SocketAddr) {
+        if let Some(_) = self.lastmsg.get(ip) {
+            let emptyvec: Vec<Vec<u8>> = Vec::new();
+            change_map_value::<SocketAddr, Vec<Vec<u8>>>(&mut self.lastmsg, *ip, emptyvec);
+            return;
+        }
+        error!("N could not be read for IP {}, probably not in map", ip);
+    }
+
+    //resend last messages
+    async fn resend_to(&mut self, ip: &SocketAddr) {
+        debug!("Resending to IP {}", ip);
+        if let Some(messages) = self.lastmsg.get(ip) {
+            for msg in messages {
+                self._send_to(ip, msg).await;
+            }
+        }
+        self.reset_n(ip);
+        self.reset_last_msg(ip);
+    }
+
+    async fn ask_resend_from_all(&mut self) {
+        warn!("Asking for resend from all IPs connected");
+        if let ipaddrs = self.get_ip_connected() {
+            for ip in ipaddrs {
+                self.resend_to(&ip);
+            }
+        }
+    }
+
+    //actual APIs used for sending and receiving data
+    pub async fn send_to(&mut self, src: &SocketAddr, message: &[u8]) {
+        self.grow_n(src);
+        self.add_last_msg(src, message.to_vec());
+        self.basic_send_to(src, message).await;
+    }
+
+    //like recv() but implements one timeout
+    pub async fn reliable_recv(&mut self, buffer: &mut [u8; MTU]) -> Option<(usize, SocketAddr)> {
+        let future = self.recv(buffer);
+        if let Ok((amt, src)) = timeout(MAX_WAIT_TIME, future).await {
+            debug!("Received packet in time");
+            Some((amt, src))
+        } else {
+            warn!("Timeout occurred, asking for resend");
+            self.ask_resend_from_all();
+            None
+        }
+    }
+
+    pub async fn recv(&mut self, buffer: &mut [u8; MTU]) -> (usize, SocketAddr) {
+        let (amt, src) = self.basic_recv(buffer).await;
+        self.grow_n(&src);
+        (amt, src)
+    }
+
+    //basic send and recv wrappers
+    async fn basic_send_to(&self, src: &SocketAddr, message: &[u8]) {
+        //TODO: use encryption_token to encrypt message before sending
+        match self.socket.send_to(message, src).await {
+            Ok(_) => debug!("Sent message"),
+            Err(_) => error!("A send error occured!"),
+        }
+    }
+
+    async fn basic_recv(&self, buffer: &mut [u8; MTU]) -> (usize, SocketAddr) {
+        let (amt, src) = self
+            .socket
+            .recv_from(&mut buffer[..])
+            .await
+            .expect("A recv error occurred!");
+        //TODO: use encryption_token to decrypt message after recv call is complete
+        (amt, src)
+    }
+}
---
+++ src/parsing.rs
@@ -0,0 +1,208 @@
+//used to convert from variables to raw bytes to be sent through network and vice versa
+use log::debug;
+use log::error;
+use std::str;
+
+use crate::protocol::MTU;
+
+//use this instead of comparing raw bytes every time
+#[derive(PartialEq)]
+pub enum PrimitiveMessage {
+    ACK,
+    NACK,
+    END,
+    RESEND,
+    INVALID,
+}
+
+//small message types are convenient as consts
+const ACK: [u8; 3] = *b"ACK";
+const NACK: [u8; 4] = *b"NACK";
+const RESEND: [u8; 6] = *b"RESEND";
+const END: [u8; 3] = *b"END";
+
+//get the plaintext parts of packet out into a String if possible
+fn parse_generic_req(message: [u8; MTU], amt: usize) -> Option<String> {
+    let req = match str::from_utf8(&message[..amt]) {
+        Ok(x) => x.to_string(),
+        Err(_) => {
+            error!("Generic parse error detected!");
+            return None;
+        }
+    };
+    Some(req)
+}
+
+//used externally to get simple message types
+pub fn get_primitive(msgtype: PrimitiveMessage) -> Vec<u8> {
+    match msgtype {
+        PrimitiveMessage::ACK => {
+            debug!("Requested ACK");
+            ACK.to_vec()
+        }
+        PrimitiveMessage::NACK => {
+            debug!("Requested NACK");
+            NACK.to_vec()
+        }
+        PrimitiveMessage::END => {
+            debug!("Requested END");
+            END.to_vec()
+        }
+        PrimitiveMessage::RESEND => {
+            debug!("Requested RESEND");
+            RESEND.to_vec()
+        }
+        _ => {
+            error!("Requested INVALID? Returning ACK");
+            ACK.to_vec()
+        }
+    }
+}
+
+//get primitive message type
+pub fn parse_primitive(message: [u8; MTU], amt: usize) -> PrimitiveMessage {
+    match amt {
+        3 => {
+            if message[..3] == ACK {
+                debug!("Parsed ACK");
+                return PrimitiveMessage::ACK;
+            }
+            if message[..3] == END {
+                debug!("Parsed END");
+                return PrimitiveMessage::END;
+            }
+            error!("Parsed invalid primitive message");
+            return PrimitiveMessage::INVALID;
+        }
+        4 => {
+            debug!("Parsed NACK");
+            return PrimitiveMessage::NACK;
+        }
+        6 => {
+            debug!("Parsed RESEND");
+            return PrimitiveMessage::RESEND;
+        }
+        _ => {
+            error!("Parsed invalid primitive message");
+            return PrimitiveMessage::INVALID;
+        }
+    }
+}
+
+//initial request client sends; consists of auth token and name of file to get
+pub fn send_req(filename: &String, auth: &String) -> Vec<u8> {
+    let r = String::from("AUTH ") + auth + &String::from("\nGET ") + filename;
+    debug!(
+        "Built send request with an authtoken and filename {}",
+        filename
+    );
+    r.as_bytes().to_vec()
+}
+
+pub fn parse_send_req(message: [u8; MTU], amt: usize) -> Option<(String, String)> {
+    let Some(req) = parse_generic_req(message, amt);
+
+    let file_requested = match req.split("GET ").collect::<Vec<&str>>().get(1) {
+        Some(x) => x.to_string(),
+        None => {
+            error!("File request parsed unsuccessfully");
+            return None;
+        }
+    };
+
+    let giventoken = match req.split("AUTH ").collect::<Vec<&str>>().get(1) {
+        Some(x) => match x.to_string().split('\n').collect::<Vec<&str>>().first() {
+            Some(y) => y.to_string(),
+            None => {
+                error!("Token parse error");
+                return None;
+            }
+        },
+        None => {
+            error!("Token parse error");
+            return None;
+        }
+    };
+    debug!("Parsed send request");
+    Some((file_requested, giventoken))
+}
+
+//server then sends filesize if everything checked out
+pub fn filesize_packet(filesize: usize) -> Vec<u8> {
+    let s = String::from("SIZE ") + &filesize.to_string();
+    debug!("Built filesize packet with size {}", filesize);
+    s.as_bytes().to_vec()
+}
+
+pub fn parse_filesize_packet(message: [u8; MTU], amt: usize) -> Option<usize> {
+    let Some(req) = parse_generic_req(message, amt);
+    let size = match req.split("SIZE ").collect::<Vec<&str>>().get(1) {
+        Some(x) => x.to_string().parse::<usize>().unwrap(),
+        None => {
+            error!("Filesize parse error");
+            return None;
+        }
+    };
+    debug!("Parsed filesize packet");
+    Some(size)
+}
+
+//here client will tell server if it wants the file or not
+
+//tell server which packet has been last received
+pub fn last_received_packet(num: usize) -> Vec<u8> {
+    let s = String::from("LAST ") + &num.to_string();
+    debug!("Built last packet with offset {}", num);
+    s.as_bytes().to_vec()
+}
+
+pub fn parse_last_received(message: [u8; MTU], amt: usize) -> Option<usize> {
+    if parse_primitive(message, amt) == PrimitiveMessage::ACK {
+        return None;
+    }
+    let Some(req) = parse_generic_req(message, amt);
+    let lastrecv = match req.split("LAST ").collect::<Vec<&str>>().get(1) {
+        Some(x) => x.to_string().parse::<usize>().unwrap(),
+        None => {
+            error!("Last receive parse error");
+            return None;
+        }
+    };
+    debug!("Parsed last received packet");
+    Some(lastrecv)
+}
+
+//data packet is basically a small header and some amount of payload
+pub fn data_packet(offset: usize, message: &Vec<u8>) -> Vec<u8> {
+    let s = String::from("OFFSET: ") + &offset.to_string() + "\n";
+    let mut b1 = s.as_bytes().to_vec();
+    b1.extend(message);
+    debug!("Built data packet with offset {}", offset);
+    b1
+}
+
+pub fn parse_data_packet(message: [u8; MTU], amt: usize) -> Option<(usize, Vec<u8>)> {
+    let mut sizeofheader = 0;
+    for i in 0..amt {
+        if message[i] == 10 {
+            sizeofheader = i + 1;
+            break;
+        }
+    }
+    let Some(req) = parse_generic_req(message, sizeofheader);
+    let offset = match req.split("OFFSET: ").collect::<Vec<&str>>().get(1) {
+        Some(x) => match x.to_string().split('\n').collect::<Vec<&str>>().first() {
+            Some(y) => y.to_string().parse::<usize>().unwrap(),
+            None => {
+                error!("Offset parse error");
+                return None;
+            }
+        },
+        None => {
+            error!("Offset parse error");
+            return None;
+        }
+    };
+    debug!("Parsed data packet");
+    Some((offset, message[sizeofheader..amt].to_vec()))
+}
