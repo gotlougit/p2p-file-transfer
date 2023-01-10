3f59dba40137978058f089b5e6b5fadd100bd5a2
Saksham Mittal <gotlouemail@gmail.com>
2023-01-10 13:36:18 +0530

    Modify server.rs to use new connection & parsing APIs

--- src/server.rs
+++ src/server.rs
@@ -3,22 +3,22 @@ use memmap2::Mmap;
 use std::collections::HashMap;
 use std::fs::OpenOptions;
 use std::net::SocketAddr;
-use tokio::net::UdpSocket;
-use tokio::time::timeout;
+use log::{debug, error, info, warn};
 
 use crate::auth;
-use crate::protocol;
-use crate::protocol::ClientState;
+use crate::parsing;
+use crate::connection;
+use crate::parsing::{ClientState,PrimitiveMessage};
 
 pub struct Server {
-    socket: UdpSocket,
+    connection: connection::Connection,
     data: Mmap,
     size_msg: Vec<u8>,
     src_state_map: HashMap<SocketAddr, ClientState>,
     authchecker: auth::AuthChecker,
 }
 
-pub fn init(socket: UdpSocket, filename: String, authtoken: String) -> Server {
+pub fn init(connection: connection::Connection, filename: String, authtoken: String) -> Server {
     let fd = OpenOptions::new()
         .read(true)
         .write(false)
@@ -29,9 +29,9 @@ pub fn init(socket: UdpSocket, filename: String, authtoken: String) -> Server {
         let mmap = Mmap::map(&fd).unwrap();
         let filesize = mmap.len();
         let server_obj = Server {
-            socket,
+            connection,
             data: mmap,
-            size_msg: protocol::filesize_packet(filesize),
+            size_msg: parsing::filesize_packet(filesize),
             src_state_map: HashMap::new(),
             authchecker: auth::init(authtoken, filename),
         };
@@ -45,31 +45,27 @@ pub fn init(socket: UdpSocket, filename: String, authtoken: String) -> Server {
 impl Server {
     pub async fn mainloop(&mut self) {
         loop {
-            let mut buf = [0u8; protocol::MTU];
-            if let Ok((amt, src)) = timeout(
-                protocol::MAX_WAIT_TIME,
-                protocol::recv(&self.socket, &mut buf),
-            )
-            .await
-            {
-                if protocol::parse_resend(buf, amt) {
-                    println!("Need to resend!");
-                    protocol::resend(&self.socket).await;
-                } else {
-                    self.process_msg(&src, buf, amt).await;
+            let mut buffer = [0u8; connection::MTU];
+            if let Some((amt,src)) = self.connection.reliable_recv(&mut buffer).await {
+                //client wants server to resend
+                if parsing::parse_primitive(&buffer, amt) == PrimitiveMessage::RESEND {
+                    warn!("Client asked for resend!");
+                    self.connection.resend_to(&src);
+                    continue;
                 }
+                //proceed normally
+                self.process_msg(&src, buffer, amt).await;
             } else {
-                println!("Timeout occurred, asking all clients to resend!");
-                //ask all clients for resend
-                self.ask_all_to_resend().await;
+                //retry again in next iteration of loop; we have already sent resend request
+                continue;
             }
         }
     }
 
     //pass message received here to determine what to do; action will be taken asynchronously
-    async fn process_msg(&mut self, src: &SocketAddr, message: [u8; protocol::MTU], amt: usize) {
+    async fn process_msg(&mut self, src: &SocketAddr, message: [u8; connection::MTU], amt: usize) {
         if self.src_state_map.contains_key(src) {
-            println!("Found prev connection, checking state and handling corresponding call..");
+            debug!("Found prev connection, checking state and handling corresponding call..");
             //we are already communicating with client
             if let Some(curstate) = self.src_state_map.get(src) {
                 match curstate {
@@ -83,24 +79,24 @@ impl Server {
                         self.send_data_in_chunks(&src, message, amt).await;
                     }
                     ClientState::EndConn => {
-                        if protocol::parse_end(message, amt) {
+                        if parsing::parse_primitive(&message[..], amt) == PrimitiveMessage::END {
                             self.end_connection(src).await;
                             return;
                         }
                         self.end_connection_with_resend(src).await;
                     }
                     ClientState::EndedConn => {
-                        if protocol::parse_end(message, amt) {
+                        if parsing::parse_primitive(&message[..], amt) == PrimitiveMessage::END {
                             self.end_connection(src).await;
                             return;
                         }
-                        if protocol::parse_resend(message, amt) {
-                            println!("Client may not have received last part of file! Sending last chunk...");
-                            let n = protocol::read_n();
-                            let mut offset = self.data.len() - protocol::DATA_SIZE * n;
+                        if parsing::parse_primitive(&message[..], amt) == PrimitiveMessage::RESEND {
+                            warn!("Client may not have received last part of file! Sending last chunk...");
+                            let n = self.connection.read_n(&src);
+                            let mut offset = self.data.len() - connection::DATA_SIZE * n;
                             for _ in 0..n {
                                 self.send_one_chunk(src, offset).await;
-                                offset += protocol::DATA_SIZE;
+                                offset += connection::DATA_SIZE;
                             }
                         } else {
                             self.send_data_in_chunks(&src, message, amt).await;
@@ -109,16 +105,10 @@ impl Server {
                 }
             }
         } else {
-            println!("New connection detected, adding to the list..");
+            info!("New connection detected, adding to the list..");
             //TODO: implement authentication check here
             self.src_state_map.insert(*src, ClientState::NoState); //start from scratch
             self.initiate_transfer_server(src, message, amt).await; //initialize function
-        }
-    }
-
-    pub async fn ask_all_to_resend(&self) {
-        for src in self.src_state_map.keys() {
-            protocol::ask_for_resend(&self.socket, src).await;
         }
     }
 
@@ -129,35 +119,35 @@ impl Server {
     }
 
     async fn end_connection(&mut self, src: &SocketAddr) {
-        println!("Sending END (permanent end to connection) to {}", src);
-        protocol::send_to(&self.socket, src, protocol::END.as_ref()).await;
+        info!("Sending END (permanent end to connection) to {}", src);
+        self.connection.send_to(&src, &parsing::get_primitive(PrimitiveMessage::END)).await;
         self.src_state_map.remove(src);
     }
 
     async fn end_connection_with_resend(&mut self, src: &SocketAddr) {
-        println!("Sending END (with resend allowed) to {}", src);
-        protocol::send_to(&self.socket, src, protocol::END.as_ref()).await;
+        info!("Sending END (with resend allowed) to {}", src);
+        self.connection.send_to(&src, &parsing::get_primitive(PrimitiveMessage::END)).await;
         self.change_src_state(src, ClientState::EndedConn);
     }
 
     async fn initiate_transfer_server(
         &mut self,
         src: &SocketAddr,
-        message: [u8; protocol::MTU],
+        message: [u8; connection::MTU],
         amt: usize,
     ) {
-        if self.authchecker.is_valid_request(message, amt) {
+        if self.authchecker.is_valid_request(&message[..], amt) {
             //send size of data
-            println!("Client authentication check succeeded...");
-            println!("Sending client size of file");
-            protocol::send_to(&self.socket, src, &self.size_msg).await;
-            println!("Awaiting response from client...");
+            info!("Client authentication check succeeded...");
+            debug!("Sending client size of file");
+            self.connection.send_to(&src, &self.size_msg).await;
+            info!("Awaiting response from client...");
             self.change_src_state(src, ClientState::ACKorNACK);
         } else {
             //send dummy message as client failed to authenticate
-            println!("Client was not able to be authenticated!");
-            println!("Sending 0 size file...");
-            protocol::send_to(&self.socket, src, &protocol::filesize_packet(0)).await;
+            error!("Client was not able to be authenticated!");
+            debug!("Sending 0 size file...");
+            self.connection.send_to(&src, &parsing::filesize_packet(0)).await;
             //end connection
             self.end_connection(src).await;
         }
@@ -166,21 +156,25 @@ impl Server {
     async fn check_ack_or_nack(
         &mut self,
         src: &SocketAddr,
-        message: [u8; protocol::MTU],
+        message: [u8; connection::MTU],
         amt: usize,
     ) {
-        if protocol::parse_ack(message, amt) {
-            println!("Client sent ACK");
-            self.change_src_state(src, ClientState::SendFile);
-            //for now we can do this directly
-            self.send_data_in_chunks(src, message, amt).await;
-        } else if protocol::parse_nack(message, amt) {
-            println!("Client sent NACK");
-            self.change_src_state(src, ClientState::EndConn);
-            //directly do this since connection needs to be closed anyway
-            self.end_connection(src).await;
-        } else {
-            println!("Client sent unknown message type for its stage! Ignoring");
+        match parsing::parse_primitive(&message[..], amt) {
+            PrimitiveMessage::ACK => {
+                debug!("Client sent ACK");
+                self.change_src_state(src, ClientState::SendFile);
+                //for now we can do this directly
+                self.send_data_in_chunks(src, message, amt).await;
+            }
+            PrimitiveMessage::NACK => {
+                debug!("Client sent NACK");
+                self.change_src_state(src, ClientState::EndConn);
+                //directly do this since connection needs to be closed anyway
+                self.end_connection(src).await;
+            }
+            _ => {
+                warn!("Client sent unknown message type for its stage! Ignoring");
+            }
         }
     }
 
@@ -188,17 +182,17 @@ impl Server {
         if offset >= self.data.len() {
             return;
         }
-        let mut len = protocol::DATA_SIZE;
+        let mut len = connection::DATA_SIZE;
         let mut end_afterwards = false;
         if offset + len > self.data.len() {
             len = self.data.len() - offset;
             end_afterwards = true;
         }
-        println!("Sending a chunk...");
+        debug!("Sending a chunk...");
         let packet = self.data[offset..offset+len].to_vec();
-        protocol::send_to(&self.socket, &src, &protocol::data_packet(offset, &packet)).await;
+        self.connection.send_to(&src, &parsing::data_packet(offset, &packet)).await;
         if end_afterwards {
-            println!("File sent completely");
+            info!("File sent completely");
             self.end_connection_with_resend(src).await;
         }
     }
@@ -206,24 +200,24 @@ impl Server {
     async fn send_data_in_chunks(
         &mut self,
         src: &SocketAddr,
-        message: [u8; protocol::MTU],
+        message: [u8; connection::MTU],
         amt: usize,
     ) {
-        if protocol::parse_end(message, amt) {
+        if parsing::parse_primitive(&message[..], amt) == PrimitiveMessage::END {
             self.end_connection(src).await;
             return;
         }
-        let mut offset = protocol::parse_last_received(message, amt);
+        let Some(mut offset) = parsing::parse_last_received(&message[..], amt);
         if offset >= self.data.len() {
             //send an END packet
             self.end_connection_with_resend(src).await;
             return;
         }
         //send PROTOCOL_N number of chunks at once and implement go back N if they have not been received
-        let n = protocol::read_n();
+        let n = self.connection.read_n(&src);
         for _ in 0..n {
             self.send_one_chunk(src, offset).await;
-            offset += protocol::DATA_SIZE;
+            offset += connection::DATA_SIZE;
         }
     }
 }
