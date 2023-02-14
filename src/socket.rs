4ce5acd93bdd33151a7cc16c1dc9bb6bf17e45e3
Saksham Mittal <gotlouemail@gmail.com>
2023-02-14 11:03:21 +0530

    Add initial code for Socket trait
    This will help us implement testing in order to make sure that the
    program works as intended for a variety of behaviours

---
+++ src/socket.rs
@@ -0,0 +1,47 @@
+//implements trait that can be used to create fake socket for testing
+use async_trait::async_trait;
+use tokio::{io::Error, net::UdpSocket};
+use std::{net::SocketAddr, str::FromStr};
+
+pub struct ActualSocket {
+    pub socket: UdpSocket,
+}
+
+pub struct DummySocket {
+    send_proper: bool,
+    recv_proper: bool,
+}
+
+#[async_trait]
+pub trait Socket {
+    async fn send_to(&self,message: &[u8], src: SocketAddr) -> Result<usize,Error>;
+    async fn recv_from(&self, message: &mut [u8]) -> Result<(usize,SocketAddr),Error>;
+}
+
+#[async_trait]
+impl Socket for ActualSocket {
+    async fn send_to(&self,message: &[u8], src: SocketAddr) -> Result<usize,Error> {
+        self.socket.send_to(message, src).await
+    }
+    async fn recv_from(&self, message: &mut [u8]) -> Result<(usize,SocketAddr),Error> {
+        self.socket.recv_from(message).await
+    }
+}
+
+#[async_trait]
+impl Socket for DummySocket {
+    async fn send_to(&self,message: &[u8], src: SocketAddr) -> Result<usize,Error> {
+        if self.send_proper {
+            Ok(message.len())
+        } else {
+            Ok(0)
+        }
+    }
+    async fn recv_from(&self, message: &mut [u8]) -> Result<(usize,SocketAddr),Error> {
+        if self.recv_proper {
+            Ok((message.len(), SocketAddr::from_str("127.0.0.1:1025").unwrap()))
+        } else {
+            Ok((0, SocketAddr::from_str("127.0.0.1:1025").unwrap()))
+        }
+    }
+}
