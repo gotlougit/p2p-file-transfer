f698171e6eb6f9045877e919e3745b38f7779191
Saksham Mittal <gotlouemail@gmail.com>
2023-10-16 10:52:16 +0530

    feat: add flake

---
+++ flake.nix
@@ -0,0 +1,29 @@
+{
+  outputs = { self, nixpkgs }:
+    let
+      system = "x86_64-linux";
+      pkgs = import nixpkgs { inherit system; };
+      mkShell = pkgs.mkShell.override { stdenv = pkgs.stdenvAdapters.useMoldLinker pkgs.stdenv; };
+    in
+    {
+      devShells.${system}.default = mkShell {
+        name = "rustdev";
+        shellHook = ''
+          export CARGO_HOME="$(realpath ./.localcargo)"
+        '';
+        buildInputs = [
+          pkgs.pkg-config
+          pkgs.openssl.dev
+          pkgs.sqlite.dev
+          pkgs.rustc
+          pkgs.cargo
+          pkgs.rustfmt
+          pkgs.clippy
+          pkgs.rust-analyzer
+          pkgs.cargo-rr
+          pkgs.tree-sitter
+        ];
+      };
+    };
+}
+
