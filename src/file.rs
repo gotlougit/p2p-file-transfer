8394998b58e127f94e5c50a0865d700de65a00b3
Saksham Mittal <git@gotlou.com>
2023-10-19 00:29:30 +0530

    fix: reading file contents into memory

--- src/file.rs
+++ src/file.rs
@@ -1,18 +1,17 @@
 use anyhow::Result;
 use tokio::{
-    fs::File,
-    io::{AsyncReadExt, AsyncWriteExt},
+    fs::{read, File},
+    io::AsyncWriteExt,
 };
 
 pub async fn get_file_contents(filename: &str) -> Result<Vec<u8>> {
-    let mut fd = File::open(filename).await?;
-    let mut buffer = Vec::new();
-    fd.read(&mut buffer).await?;
-    Ok(buffer)
+    let contents = read(filename).await?;
+    Ok(contents)
 }
 
 pub async fn dump_to_file(filename: &str, bytes: &[u8]) -> Result<()> {
     let mut fd = File::create(filename).await?;
-    fd.write(bytes).await?;
+    fd.write_all(bytes).await?;
+    fd.flush().await?;
     Ok(())
 }
