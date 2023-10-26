849b6f66c027d83cb5cac516c7b4ce0453bc96ed
Saksham Mittal <git@gotlou.com>
2023-10-26 14:20:42 +0530

    feat: use clap for parsing CLI args

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,6 @@
 use crate::config::Config;
 use anyhow::Result;
-use std::env;
+use clap::{Parser, Subcommand};
 
 mod client;
 mod config;
@@ -8,37 +8,62 @@ mod file;
 mod nat;
 mod server;
 
+#[derive(Subcommand, PartialEq)]
+enum Command {
+    /// Request a file from a server
+    Client {
+        #[arg(required = true)]
+        filename: String,
+        #[arg(required = true)]
+        auth: String,
+    },
+    /// Host a server
+    Server {
+        #[arg(required = true)]
+        filename: String,
+        #[arg(required = true)]
+        auth: String,
+    },
+    /// Add a certificate to key store
+    AddKey {
+        #[arg(required = true)]
+        key: String,
+    },
+}
+
+/// Transfer files using UDP hole punching
+#[derive(Parser)]
+#[command(author, version, about, long_about = None)]
+struct Args {
+    #[command(subcommand)]
+    pub command: Option<Command>,
+}
+
 #[tokio::main]
 async fn main() -> Result<()> {
     tracing_subscriber::fmt::init();
-    let Config {
-        public_key,
-        private_key,
-        trusted_keys,
-    } = crate::config::get_all_vars()?;
-    // TODO: use clap crate for CLI arguments
-    let args: Vec<String> = env::args().collect();
-    if args.len() != 4 {
-        eprintln!("Insufficient args entered! Usage: ./program client <file_to_get> or ./program server <file_to_serve> <pre_shared_secret>");
-    }
-
-    // client mode or server mode
-    let mode = &args[1];
-    // pre shared secret which acts as a deterrent against MITM attacks
-    let auth = &args[3];
-
-    if mode == "server" {
-        let filename = &args[2];
-        let (serversock, _addr) = crate::nat::get_nat_traversed_socket().await.unwrap();
-        crate::server::run_server(serversock, filename, auth, public_key, private_key).await?;
-    } else if mode == "client" {
-        let file_to_get = &args[2];
-        let (sock, server_addr) = crate::nat::get_nat_traversed_socket().await.unwrap();
-        crate::client::run_client(sock, server_addr, file_to_get, auth, trusted_keys)
-            .await
-            .unwrap();
-    } else {
-        eprintln!("Incorrect args entered!");
+    let args = Args::parse();
+    if let Some(cmd) = args.command {
+        let Config {
+            public_key,
+            private_key,
+            trusted_keys,
+        } = crate::config::get_all_vars()?;
+        match cmd {
+            Command::Server { filename, auth } => {
+                let (serversock, _addr) = crate::nat::get_nat_traversed_socket().await.unwrap();
+                crate::server::run_server(serversock, &filename, &auth, public_key, private_key)
+                    .await?;
+            }
+            Command::Client { filename, auth } => {
+                let (sock, server_addr) = crate::nat::get_nat_traversed_socket().await.unwrap();
+                crate::client::run_client(sock, server_addr, &filename, &auth, trusted_keys)
+                    .await?;
+            }
+            Command::AddKey { key } => {
+                todo!();
+            }
+        };
     }
     Ok(())
 }
